d4e395c57a0ac08af93ca15bbbc7207cf43ac13a
Elizaveta Biryukova <birliza@mail.ru>
2022-09-12 13:20:20 +0200

    Adds slider js

--- src/js/modules/slider.js
+++ src/js/modules/slider.js
@@ -3,12 +3,23 @@ const sliders = document.querySelectorAll('.testimonials__slider');
 const toggle = document.querySelector('.testimonials__button');
 const toggles = document.querySelectorAll('.testimonials__button');
 
+const togglesArray = Array.from(toggles);
 
-toggles.forEach((el) => {
-    el.addEventListener('click', () => {
-        toggles.forEach(item => {
+let index = 0;
+
+togglesArray.forEach((el) => {
+    el.addEventListener('click', (event) => {
+        togglesArray.forEach(item => {
             item.classList.remove('testimonials__button--active');
+
         });
         el.classList.add('testimonials__button--active');
+        const index = togglesArray.indexOf(event.target);
+        sliders.forEach(item => {
+            item.classList.remove('testimonials__slider--active');
+
+        });
+        sliders[index].classList.add('testimonials__slider--active');
     });
+
 });
