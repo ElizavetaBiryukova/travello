a9116103432068a5d27e8e12bacbd11346b46856
Elizaveta Biryukova <birliza@mail.ru>
2022-09-20 15:07:16 +0200

    Adds filter

--- src/js/modules/countries.js
+++ src/js/modules/countries.js
@@ -1,7 +1,9 @@
 const countriesList = document.querySelector('.countries__list');
 const buttons = document.querySelectorAll('.countries__button');
 const hotels = document.querySelectorAll('.hotels__item');
-const viewAll = document.querySelector('.gallery__button');
+const viewAll = document.querySelector('.gallery__view-all');
+const up = document.querySelector('.gallery__up');
+const nothingFound = document.querySelector('.gallery__nothing-found');
 
 const filter = () => {
     countriesList.addEventListener('click', (event) => {
@@ -39,23 +41,55 @@ const filter = () => {
                 getItems('hotels__item--nepal');
                 break;
         }
+
+        viewAll.classList.remove('visually-hidden');
+        up.classList.ad('visually-hidden');
     });
 
     viewAll.addEventListener('click', () => {
         hotels.forEach(item => {
-            item.style.display = 'block';
+            item.classList.remove('block-none');
+            item.classList.add('block');
+
         });
+        buttons.forEach(item => {
+            item.classList.remove('countries__button--active');
+        });
+        viewAll.classList.add('visually-hidden');
+        up.classList.remove('visually-hidden');
+        nothingFound.classList.add('visually-hidden');
+        countriesList.scrollIntoView();
     });
+
+
+    up.addEventListener('click', () => {
+        countriesList.scrollIntoView();
+    });
+
 };
 
 filter();
 
 const getItems = (className) => {
+
     hotels.forEach(item => {
-        if (item.classList.contains(className) == true) {
-            item.style.display = 'block';
+        if (item.classList.contains(className)) {
+            item.classList.remove('block-none');
+            item.classList.add('block');
         } else {
-            item.style.display = 'none';
+            item.classList.remove('block');
+            item.classList.add('block-none');
         }
     });
+
+    let hotelsArray = Array.from(hotels);
+    console.log(hotelsArray);
+    console.log(hotelsArray.find(el => el.classList.contains('block')));
+    if (hotelsArray.find(el => el.classList.contains('block')) == undefined) {
+        nothingFound.classList.remove('visually-hidden');
+    } else {
+        nothingFound.classList.add('visually-hidden');
+    }
+
+
 };
