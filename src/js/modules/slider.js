7e65e8478029ec9b9b72494cc298ebe618d2c36f
Elizaveta Biryukova <birliza@mail.ru>
2022-09-28 20:19:15 +0200

    Change slider

--- src/js/modules/slider.js
+++ src/js/modules/slider.js
@@ -7,19 +7,53 @@ const togglesArray = Array.from(toggles);
 
 let index = 0;
 
+const changeButton = (el) => {
+    togglesArray.forEach(item => {
+        item.classList.remove('testimonials__button--active');
+    });
+    el.classList.add('testimonials__button--active');
+};
+
+const changeSlider = (index) => {
+    sliders.forEach(item => {
+        item.classList.remove('testimonials__slider--active');
+    });
+    sliders[index].classList.add('testimonials__slider--active');
+};
+
 togglesArray.forEach((el) => {
     el.addEventListener('click', (event) => {
-        togglesArray.forEach(item => {
-            item.classList.remove('testimonials__button--active');
-
-        });
-        el.classList.add('testimonials__button--active');
         const index = togglesArray.indexOf(event.target);
-        sliders.forEach(item => {
-            item.classList.remove('testimonials__slider--active');
-
-        });
-        sliders[index].classList.add('testimonials__slider--active');
+        changeButton(el);
+        changeSlider(index);
     });
-
 });
+
+
+
+const timeChange = () => {
+    for (index = 0; index < togglesArray.length; index++) {
+
+        changeButton(togglesArray[index]);
+        changeSlider(index);
+    }
+};
+
+// timeChange();
+// const timeChange = () => {
+//     for (let i = 0; i < togglesArray.length - 1; i++) {
+//         // console.log(index++);
+//         changeButton(togglesArray[index]);
+//         changeSlider(index);
+//         index++;
+//     }
+//     // console.log(index);
+
+// };
+
+// // if ()
+
+// console.log(timeChange());
+// // console.log(togglesArray[2]);
+setTimeout(timeChange, 2000);
+// setInterval(timeChange, 2000);
